2862a9776c92939e3aecccbd2cc3bd3f76940e4f
FranzCalore <franzkaiser1993@gmail.com>
2022-11-18 12:50:30 +0100

    terminato l'esercizio

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let codiceSconto = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
+let portfolio = [{
+    nomeSito : "Cabin Website",
+    img : "cabin.png"
+}, {
+    nomeSito : "Cake Website",
+    img : "cake.png"
+}, {
+    nomeSito : "Circus Website",
+    img : "circus.png"
+}, {
+    nomeSito : "Game Website",
+    img : "game.png"
+}, {
+    nomeSito : "Safe Website",
+    img : "safe.png"
+}, {
+    nomeSito : "Submarine Website",
+    img : "submarine.png"
+}]
 
 
+printPortfolio(portfolio);
 
 
 
@@ -24,13 +44,15 @@ function submitForm(event){
 
 
 function applicaSconto(prezzoBase, codiceInserito){
-    for (sconto in codiceSconto){
+    for (sconto of codiceSconto){
         if (sconto == codiceInserito){
+            document.getElementById("inputDiscount").classList.remove("text-danger")
             let indiceSconto=codiceSconto.indexOf(sconto);
             codiceSconto.splice(indiceSconto,1)
             return prezzoScontato = prezzoBase*0.75
         }
         document.getElementById("inputDiscount").classList.add("text-danger")
+        alert("CODICE SCONTO ERRATO O SCADUTO");
         return prezzoBase
     }
 }
@@ -49,4 +71,24 @@ function calcoloPrezzo (){
     let oreLavoro=document.getElementById("hoursRequested").value
     let costo= oreLavoro* lavoro
     return costo
+}
+
+
+function printPortfolio(Portfolio){
+    for (webSite of Portfolio){
+        document.getElementById("portfolio").innerHTML+= 
+        `<div class="col-12 col-md-6 col-lg-4">
+        <div class="card shadow p-3 mb-5 bg-white rounded">
+            <img src="./img/portfolio/${webSite.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h3 class="card-title">${webSite.nomeSito}</h3>
+            </div>
+            <div class="card-body">
+                <button type="button" class="btn btn-lg btn-info btn mx-3">Preview</button>
+                <button type="button" class="btn btn-lg btn-outline-info">Visit Site</button>
+            </div>
+        </div>
+
+    </div>`
+    }
 }
